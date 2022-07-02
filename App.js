24232ccefaf79bae49300eb2bd476b34d7073d27
ammannn <memandeep7@gmail.com>
2022-07-03 03:44:13 +0530

    update

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./screens/Login";
-import HomeScreen from "./screens/Home";
-import SignUpScreen from "./screens/SignUp";
-import firebase from "firebase/app";
-import "firebase/auth";
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/Login';
+import HomeScreen from './screens/Home';
+import SignUpScreen from './screens/SignUp';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import {
   Text,
   Link,
@@ -19,19 +19,19 @@ import {
   extendTheme,
   VStack,
   Box,
-} from "native-base";
+} from 'native-base';
 
-import Deals from "./view/Deals";
-import { NavBar } from "./components/NavBar";
-import Welcome from "./screens/Welcome";
-import { nativeBaseHackTheme } from "./theme/NativeBaseHackTheme";
-import DetailsDeal from "./components/DetailDeal";
+import Deals from './view/Deals';
+import { NavBar } from './components/NavBar';
+import Welcome from './screens/Welcome';
+import { nativeBaseHackTheme } from './theme/NativeBaseHackTheme';
+import DetailsDeal from './components/DetailDeal';
 
 const Stack = createNativeStackNavigator();
 // Define the config
 const config = {
   useSystemColorMode: false,
-  initialColorMode: "dark",
+  initialColorMode: 'dark',
 };
 
 // extend the theme
@@ -41,12 +41,12 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   const firebaseConfig = {
-    apiKey: "AIzaSyAYL85a8H6c-_9fU4OtDE6LLTkVOJJIqAg",
-    authDomain: "native-base-hack.firebaseapp.com",
-    projectId: "native-base-hack",
-    storageBucket: "native-base-hack.appspot.com",
-    messagingSenderId: "600550657392",
-    appId: "1:600550657392:web:b4d14f69840385f6d613b5",
+    apiKey: 'AIzaSyAYL85a8H6c-_9fU4OtDE6LLTkVOJJIqAg',
+    authDomain: 'native-base-hack.firebaseapp.com',
+    projectId: 'native-base-hack',
+    storageBucket: 'native-base-hack.appspot.com',
+    messagingSenderId: '600550657392',
+    appId: '1:600550657392:web:b4d14f69840385f6d613b5',
   };
 
   //Checking if firebase has been initialized
@@ -58,7 +58,7 @@ function App() {
 
   firebase.auth().onAuthStateChanged((user) => {
     if (user != null) {
-      console.log("##########################");
+      console.log('##########################');
       setIsLoggedIn(true);
       setLoading(false);
     } else {
@@ -83,6 +83,11 @@ function App() {
           <Stack.Navigator>
             <Stack.Screen
               name="Home"
+              component={Deals}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="DetailsDeal"
               component={DetailsDeal}
               options={{ headerShown: false }}
             />
--- view/Deals.js
+++ view/Deals.js
@@ -14,60 +14,60 @@ import {
   Flex,
   Icon,
   ScrollView,
-} from "native-base";
-import { NativeBaseHackButtonGroup } from "../components/Buttons";
-import Card from "../components/Card";
-import NativeBaseHackSelect from "../components/DropDown";
-import { Ionicons, FontAwesome } from "@expo/vector-icons";
-import { NavBar } from "../components/NavBar";
-import { SafeAreaView } from "react-native-safe-area-context";
-import AppBar from "../components/AppBar";
-const Deals = () => {
+} from 'native-base';
+import { NativeBaseHackButtonGroup } from '../components/Buttons';
+import Card from '../components/Card';
+import NativeBaseHackSelect from '../components/DropDown';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
+import { NavBar } from '../components/NavBar';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import AppBar from '../components/AppBar';
+const Deals = ({ navigation }) => {
   let { colorMode } = useColorMode();
   let card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   console.log(colorMode);
   return (
     <Box
       // safeAreaTop
-      width={"100%"}
-      bg={colorMode === "light" ? "primary.300" : "blueGray.900"}
+      width={'100%'}
+      bg={colorMode === 'light' ? 'primary.300' : 'blueGray.900'}
     >
       <AppBar />
       <ScrollView _contentContainerStyle={{}}>
         {/* <NavBar /> */}
         <Box
-          bg={colorMode === "light" ? "coolGray.100" : "coolGray.900"}
+          bg={colorMode === 'light' ? 'coolGray.100' : 'coolGray.900'}
           w="100%"
           justifyContent="center"
-          alignItems={"center"}
+          alignItems={'center'}
         >
-          <Container minW={"95%"} justifyContent="center">
+          <Container minW={'95%'} justifyContent="center">
             <Box>
               <VStack marginTop={10}>
                 <Heading>Grab Live Deals</Heading>
-                <Heading fontSize={"sm"}>
+                <Heading fontSize={'sm'}>
                   Invest into leading projects and get a great returns
                 </Heading>
               </VStack>
 
-              <Box w={"100%"}>
+              <Box w={'100%'}>
                 <HStack
                   w="100%"
-                  flexDir={"row"}
+                  flexDir={'row'}
                   justifyContent="flex-start"
                   py={1}
                 >
                   <Box flex={2}>
-                    <Box w={"100%"} alignItems={"center"}>
+                    <Box w={'100%'} alignItems={'center'}>
                       <HStack
-                        width={{ md: "65%" }}
-                        flexDir={"row"}
+                        width={{ md: '65%' }}
+                        flexDir={'row'}
                         justifyContent="flex-start"
                         py={10}
                       >
                         <Box flex={2}>
-                          <InputGroup w={"100%"}>
-                            <Input w={"80%"} placeholder="Search for Deals" />
+                          <InputGroup w={'100%'}>
+                            <Input w={'80%'} placeholder="Search for Deals" />
                             <InputRightAddon
                               children={
                                 <Icon as={Ionicons} name="search" size={5} />
@@ -81,16 +81,16 @@ const Deals = () => {
                       </HStack>
                       <Center>
                         <NativeBaseHackButtonGroup
-                          label={["All", "Public", "Private"]}
+                          label={['All', 'Public', 'Private']}
                         />
                       </Center>
                       <HStack
-                        width={{ md: "80%" }}
+                        width={{ md: '80%' }}
                         justifyContent="space-between"
-                        flexWrap={"wrap"}
+                        flexWrap={'wrap'}
                       >
                         {card.map((i, v) => {
-                          return <Card />;
+                          return <Card navigation={navigation} />;
                         })}
                       </HStack>
                     </Box>
