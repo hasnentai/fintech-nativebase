5f86838ba972c8f5c05eddd932184a65f3f020a2
hasnentai <hasnen712@live.com>
2022-07-03 07:16:35 +0530

    new coded

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import { Box, Center, Heading } from "native-base";
 
 const Footer = () => {
   return (
-    <Center>
-      <Box>
+    <Center width={"100%"}>
+      <Box borderWidth={1} width={"100%"}>
         <Heading>
           BuildWith NativeBase at #HackingWithNativeBase Hackathon
         </Heading>{" "}
